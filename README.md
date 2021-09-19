524a892de1c30054e7b232a88831e6873aecac38
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-09-19 11:59:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Travelperk GTK
+
+This is just a proof of concept for connecting a GTK UI with [travelperk-http-python](https://github.com/namelivia/travelperk-http-python/).
+Dependencies need to be installed globally and urrently it only supports the creation of cost centers.
+To execute just run `./run`.
+
+Before getting started retrieving querying information from the TravelPerk Web API you first need to [get an API Key](https://developers.travelperk.com/reference#authentication).
+Then put your API key on the following path: `~/.travelperk/credentials`
+
---
+++ app/travelperk.py
@@ -0,0 +1,85 @@
+from gi.repository import Gtk
+from backend.travelperk import get_backend
+import gi
+gi.require_version("Gtk", "3.0")
+
+
+class CreateCostCenterBox:
+
+    def create_cost_center(self, ref):
+        name = self.cost_center_input.get_text()
+        try:
+            cost_center = get_backend().cost_centers().cost_centers().create(name)
+            dialog = Gtk.MessageDialog(
+                transient_for=self.window,
+                flags=0,
+                message_type=Gtk.MessageType.INFO,
+                buttons=Gtk.ButtonsType.OK,
+                text=f"The new cost center {cost_center.name} has been sucessfully created",
+            )
+            dialog.run()
+            dialog.destroy()
+        except Exception as e:
+            dialog = Gtk.MessageDialog(
+                transient_for=self.window,
+                flags=0,
+                message_type=Gtk.MessageType.ERROR,
+                buttons=Gtk.ButtonsType.CANCEL,
+                text="Error creating cost center",
+            )
+            dialog.format_secondary_text(str(e))
+            dialog.run()
+            dialog.destroy()
+
+    def _create_box(self):
+        box = Gtk.Box()
+        box.set_orientation(Gtk.Orientation.VERTICAL)
+        box.set_border_width(10)
+        box.set_spacing(6)
+        return box
+
+    def _create_cost_center_label(self, box):
+        serial_label = Gtk.Label()
+        serial_label.set_text('Name')
+        box.add(serial_label)
+
+    def _create_cost_center_input(self, box):
+        self.cost_center_input = Gtk.Entry()
+        self.cost_center_input.props.xalign = 0.5
+        box.add(self.cost_center_input)
+
+    def _create_create_button(self, box):
+        create_button = Gtk.Button("Create")
+        create_button.connect('clicked', self.create_cost_center)
+        box.add(create_button)
+
+    def get(self):
+        return self.box
+
+    def __init__(self, window):
+        self.box = self._create_box()
+        self.window = window
+        self._create_cost_center_label(self.box)
+        self._create_cost_center_input(self.box)
+        self._create_create_button(self.box)
+
+
+class TravelPerk:
+    def _create_window(self):
+        window = Gtk.Window()
+        window.set_title(
+            "TravelPerk"
+        )
+        window.set_position(Gtk.WindowPosition.CENTER)
+        window.connect("destroy", Gtk.main_quit)
+        return window
+
+    def main(self):
+        window = self._create_window()
+        window.add(CreateCostCenterBox(window).get())
+        window.show_all()
+        Gtk.main()
+
+
+if __name__ == "__main__":
+    TravelPerk().main()
